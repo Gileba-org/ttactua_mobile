a607e1399792b27fa32a0a813a532fc716eb053c
Gileba <gileba@MacBook-Air-van-Gijs.local>
2016-04-28 19:14:33 +0200

    Add col-s to all positions

--- index.php
+++ index.php
@@ -58,17 +58,17 @@
 						<p><a id="simple-menu" href="#sidr"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/images/menu.gif" height="31px" width="34px" /></a></p>
 					</div>
 			</div>
-				<div class="col-12 col-m-12"><jdoc:include type="modules" name="title" /></div>
 			<div class="row" id="title">
+				<div class="col-12 col-s-12 col-m-12"><jdoc:include type="modules" name="title" /></div>
 			</div>
 			<div class="row">
-				<div class="col-12 col-m-12"><jdoc:include type="modules" name="top" /></div>
+				<div class="col-12 col-s-12 col-m-12"><jdoc:include type="modules" name="top" /></div>
 			</div>
 			<div class="row">
-				<div class="col-12 col-m-12"><jdoc:include type="modules" name="banners" /></div>
+				<div class="col-12 col-s-12 col-m-12"><jdoc:include type="modules" name="banners" /></div>
 			</div>
 			<div class="row">
-				<div class="col-12 col-m-12"><jdoc:include type="modules" name="header" /></div>
+				<div class="col-12 col-s-12 col-m-12"><jdoc:include type="modules" name="header" /></div>
 			</div>
 			<div class="row">
 				<div class="col-3 col-s-4 col-m-4 col-p-0"><jdoc:include type="modules" name="left" /></div>
@@ -76,7 +76,7 @@
 				<div class="col-3 col-s-0 col-m-0"><jdoc:include type="modules" name="right" /></div>
 			</div>			 
 			<div class="row" id="footer">
-				<div class="col-12 col-m-12"><jdoc:include type="modules" name="footer" /></div>
+				<div class="col-12 col-s-12 col-m-12"><jdoc:include type="modules" name="footer" /></div>
 			</div>
 		</div>
     <!-- Your code -->
