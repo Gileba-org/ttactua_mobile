da43672cb8d739f896f0617ebd57187b129511b8
Gileba <gileba@MacBook-Air-van-Gijs.local>
2016-04-29 12:01:06 +0200

    Every row has an ID (including subs)

--- index.php
+++ index.php
@@ -61,18 +61,18 @@
 			<div class="row" id="title">
 				<div class="col-12 col-s-12 col-m-12"><jdoc:include type="modules" name="title" /></div>
 			</div>
-			<div class="row">
+			<div class="row" id="top">
 				<div class="col-12 col-s-12 col-m-12"><jdoc:include type="modules" name="top" /></div>
 			</div>
-			<div class="row">
+			<div class="row" id="banners">
 				<div class="col-12 col-s-12 col-m-12"><jdoc:include type="modules" name="banners" /></div>
 			</div>
-			<div class="row">
+			<div class="row" id="header">
 				<div class="col-12 col-s-12 col-m-12"><jdoc:include type="modules" name="header" /></div>
 			</div>
 			<div class="row" id="main">
-				<div class="col-3 col-s-4 col-m-4 col-p-0"><jdoc:include type="modules" name="left" /></div>
-				<div class="col-6 col-s-8 col-m-8"><jdoc:include type="component" /></div>
+				<div class="col-3 col-s-4 col-m-4 col-p-0" id="left"><jdoc:include type="modules" name="left" /></div>
+				<div class="col-6 col-s-8 col-m-8" id="component"><jdoc:include type="component" /></div>
 				<div class="col-3 col-s-0 col-m-0 col-p-0" id="right"><jdoc:include type="modules" name="right" /></div>
 			</div>			 
 			<div class="row" id="footer">
