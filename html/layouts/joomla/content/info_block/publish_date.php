05d6ce2ad1e1dcbe620bb3a0f1498bd02927cd20
Gileba <gileba@MacBook-Air-van-Gijs.local>
2016-05-11 19:50:23 +0200

    no icon for date

--- html/layouts/joomla/content/info_block/publish_date.php
+++ html/layouts/joomla/content/info_block/publish_date.php
@@ -10,7 +10,6 @@
 defined('JPATH_BASE') or die;
 ?>
 			<dd class="published">
-				<span class="icon-calendar"></span>
 				<time datetime="<?php echo JHtml::_('date', $displayData['item']->publish_up, 'c'); ?>" itemprop="datePublished">
 					<?php echo JText::sprintf(JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3'))); ?>
 				</time>
