8d8a9222621bc155d07c6048da8b3a10bb16d345
Gileba <gileba@MacBook-Air-van-Gijs.local>
2016-05-26 23:49:28 +0200

    NEW: Parameter to change date format
    Closes #154

--- html/layouts/joomla/content/info_block/publish_date.php
+++ html/layouts/joomla/content/info_block/publish_date.php
@@ -7,10 +7,17 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+	$app      = JFactory::getApplication();
+	$params	  = $app->getTemplate(true)->params;
+
 defined('JPATH_BASE') or die;
 ?>
 			<dd class="published">
 				<time datetime="<?php echo JHtml::_('date', $displayData['item']->publish_up, 'c'); ?>" itemprop="datePublished">
-					<?php echo JText::sprintf(JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3'))); ?>
+					<?php if ($params->get('specificDateFormat') != "") {
+						echo JText::sprintf(JHtml::_('date', $displayData['item']->publish_up, JText::_($params->get('specificDateFormat'))));
+					} else {
+						echo JText::sprintf(JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3')));
+					}	?>						
 				</time>
 			</dd>
