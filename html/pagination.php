cbed0a9e2c220edfacedc818b59642e60779f111
Gileba <info@gileba.be>
2017-05-24 23:32:03 +0200

    Fix merge of two pagination requests

--- html/pagination.php
+++ html/pagination.php
@@ -1,13 +1,113 @@
 <?php
 /**
+ * @package     Joomla.Site
+ * @subpackage  Templates.protostar
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Renders an active item in the pagination block
+ *
+ * @param   JPaginationObject  $item  The current pagination object
+ *
+ * @return  string                    HTML markup for active item
+ *
+ * @since   3.0
+ */
+function pagination_item_active(&$item)
+{
+	$class = '';
+
+	// Check for "Start" item
+	if ($item->text == JText::_('JLIB_HTML_START'))
+	{
+		$display = '<span class="icon-first"></span>';
+	}
+
+	// Check for "Prev" item
+	if ($item->text == JText::_('JPREV'))
+	{
+		$display = '<span class="icon-previous"></span>';
+	}
+
+	// Check for "Next" item
+	if ($item->text == JText::_('JNEXT'))
+	{
+		$display = '<span class="icon-next"></span>';
+	}
+
+	// Check for "End" item
+	if ($item->text == JText::_('JLIB_HTML_END'))
+	{
+		$display = '<span class="icon-last"></span>';
+	}
+
+	// If the display object isn't set already, just render the item with its text
+	if (!isset($display))
+	{
+		$display = $item->text;
+		$class   = ' class="hidden-phone"';
+	}
+
+	return '<li' . $class . '><a title="' . $item->text . '" href="' . $item->link . '" class="pagenav">' . $display . '</a></li>';
+}
+
+/**
+ * Renders an inactive item in the pagination block
+ *
+ * @param   JPaginationObject  $item  The current pagination object
+ *
+ * @return  string  HTML markup for inactive item
+ *
+ * @since   3.0
+ */
+function pagination_item_inactive(&$item)
+{
+	// Check for "Start" item
+	if ($item->text == JText::_('JLIB_HTML_START'))
+	{
+		return '<li class="disabled"><a><span class="icon-first"></span></a></li>';
+	}
+
+	// Check for "Prev" item
+	if ($item->text == JText::_('JPREV'))
+	{
+		return '<li class="disabled"><a><span class="icon-previous"></span></a></li>';
+	}
+
+	// Check for "Next" item
+	if ($item->text == JText::_('JNEXT'))
+	{
+		return '<li class="disabled"><a><span class="icon-next"></span></a></li>';
+	}
+
+	// Check for "End" item
+	if ($item->text == JText::_('JLIB_HTML_END'))
+	{
+		return '<li class="disabled"><a><span class="icon-last"></span></a></li>';
+	}
+
+	// Check if the item is the active page
+	if (isset($item->active) && $item->active)
+	{
+		return '<li class="active hidden-phone"><span class="pagenav">' . $item->text . '</span></li>';
+	}
+
+	// Doesn't match any other condition, render a normal item
+	return '<li class="disabled hidden-phone"><a>' . $item->text . '</a></li>';
+}
+
+/**
  * By default joomla display 10 pages in pagination. This file allows
  * us to customize the number of displayed pages.
  * 
  * Simply add this file to $template/html/pagination.php and
  * change $displayedPages variable.
  */
-defined('_JEXEC') or die('Restricted access');
-
 
 /**
  * Override joomla default pagination list render method
