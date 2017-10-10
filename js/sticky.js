7c7df82b87c7a5c6e88fad3d1d09405794b321f4
Gileba <info@gileba.be>
2017-10-10 11:41:23 +0200

    Mixed tabs and spaces

--- js/sticky.js
+++ js/sticky.js
@@ -1,24 +1,25 @@
 var win = jQuery(window);
 
 function moveScroller() {
-    var move = function() {
-	    var stickyAnchor = jQuery("#sticky-anchor")
-        if ((win.scrollTop()) > (stickyAnchor.offset().top)) {
-            if (win.width() >= 720) {
-	            jQuery("#sticky").addClass("stick");
+	var move = function() {
+		var stickyAnchor = jQuery("#sticky-anchor")
+
+		if ((win.scrollTop()) > (stickyAnchor.offset().top)) {
+			if (win.width() >= 720) {
+				jQuery("#sticky").addClass("stick");
 				stickyAnchor.height(jQuery("#sticky").outerHeight());
 			} else {
 				jQuery(".flip-container").addClass("hover");
 			}			
-        } else {
-            if (win.width() >= 720) {
-                jQuery("#sticky").removeClass("stick");
-				stickyAnchor.height(0);
-			} else {
-				jQuery(".flip-container").removeClass("hover");					
+		} else {
+			if (win.width() >= 720) {
+				jQuery("#sticky").removeClass("stick");
+					stickyAnchor.height(0);
+				} else {
+					jQuery(".flip-container").removeClass("hover");					
+				}
 			}
-        }
-    };
-    win.scroll(move);
-    move();
+		};
+	win.scroll(move);
+	move();
 }
