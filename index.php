711535e236702b1c0fea872c9ed874efc7aeddca
Gijs Lamon <Gileba@users.noreply.github.com>
2018-05-03 15:09:33 +0200

    Hide analytics when no ID (#486)
    Closes #485

--- index.php
+++ index.php
@@ -147,6 +147,7 @@
 		<script>
 			moveScroller();
 		</script>
+<?php	if ($this->params->get('analytics')) {	?>
 		<!-- Google Analytics -->
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -157,5 +158,6 @@
   			ga('create', '<?php	echo $this->params->get('analytics'); ?>', 'auto');
   			ga('send', 'pageview');
   		</script>
+<?php	}	?>
 	</body>
 </html>
