bbc77c8d1a04e319d34dcf505229868f956d2dc9
Gileba <info@gileba.be>
2019-05-30 01:08:21 +0200

    Another test

--- html/com_content/article/default.php
+++ html/com_content/article/default.php
@@ -98,8 +98,10 @@ if ($tplParams->get('swipe')) {
 		<?php if (strtotime($this->item->publish_up) > strtotime(JFactory::getDate())) : ?>
 			<span class="label label-warning"><?php echo JText::_('JNOTPUBLISHEDYET'); ?></span>
 		<?php endif; ?>
-		<?php if ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate()))
-		&& ($this->item->publish_down != JFactory::getDbo()->getNullDate())) : ?>
+		<?php 
+			if ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate()))
+			&& ($this->item->publish_down != JFactory::getDbo()->getNullDate())) : 
+		?>
 			<span class="label label-warning"><?php echo JText::_('JEXPIRED'); ?></span>
 		<?php endif; ?>
 	</div>
