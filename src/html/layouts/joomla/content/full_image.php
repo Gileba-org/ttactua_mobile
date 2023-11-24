9d306965f95aff79019b4b3cb1e8e7c11111fe52
Gijs Lamon <Gileba@users.noreply.github.com>
2023-11-24 21:06:31 +0100

    Stop using the full image (#769)

--- src/html/layouts/joomla/content/full_image.php
+++ src/html/layouts/joomla/content/full_image.php
@@ -6,16 +6,18 @@
  * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die;
+defined("_JEXEC") or die();
 $params = $displayData->params;
 ?>
 <?php $images = json_decode($displayData->images); ?>
-<?php if (!empty($images->image_fulltext)) : ?>
-	<?php $imgfloat = empty($images->float_fulltext) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
+<?php if (!empty($images->image_intro)): ?>
+	<?php $imgfloat = empty($images->float_fulltext) ? $params->get("float_fulltext") : $images->float_fulltext; ?>
 	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image full"> <img
-		<?php if (isset($images->image_fulltext_caption) && $images->image_fulltext_caption) :
-			echo 'class="caption" title="' . htmlspecialchars($images->image_fulltext_caption) . '"';
-		endif; ?>
-	src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>"
+		<?php if (isset($images->image_intro_caption) && $images->image_intro_caption):
+  	echo 'class="caption" title="' . htmlspecialchars($images->image_intro_caption) . '"';
+  endif; ?>
+	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars(
+	$images->image_intro_alt
+); ?>"
 	itemprop="image"/> </div>
 <?php endif;
